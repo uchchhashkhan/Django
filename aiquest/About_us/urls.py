6256c64e9f01981240d43727b9ae604d2669a6a7
uchchhashkhan <uchchhashkhan6@gmail.com>
2023-08-06 22:52:59 +0600

    12th commit

--- aiquest/About_us/urls.py
+++ aiquest/About_us/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('a/',views.about,name='about'),
+    path('t/',views.teachers_info)
 ]
--- aiquest/About_us/views.py
+++ aiquest/About_us/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from About_us.models import Teachers
 
 # Create your views here.
 def about(request):
     return render(request,'about/about.html')
+
+def teachers_info(request):
+    teach= Teachers.objects.all()
+    return render(request,'about/t.html', {'thr': teach})
