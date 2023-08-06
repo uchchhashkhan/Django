97179c026cf3adb2f82eaeb78382b37cca3bfb66
uchchhashkhan <uchchhashkhan6@gmail.com>
2023-08-06 12:47:04 +0600

    11th commit

--- aiquest/About_us/models.py
+++ aiquest/About_us/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Teachers(models.Model):
+    tid = models.IntegerField()
+    tname = models.CharField(max_length=40)
+    temail = models.EmailField(max_length=30)
