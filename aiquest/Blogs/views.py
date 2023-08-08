57cbf279a8e27ae058b2d04339f2265b2c4dc17e
uchchhashkhan <uchchhashkhan6@gmail.com>
2023-08-08 22:59:39 +0600

    17th commit

--- aiquest/Blogs/views.py
+++ aiquest/Blogs/views.py
@@ -7,5 +7,11 @@ def blog1(request):
     return render(request,'blogs/blogs.html')
 
 def showformsdata(request):
+    if request.method == 'POST':
+        fm=TeachersRegistration(request.POST)
+        print(fm)
+        print('This is post statement')
+        print(fm.cleaned_data)
     fm=TeachersRegistration()    
+    print('This is get statement')
     return render(request, 'blogs/forms.html',{'form': fm})
