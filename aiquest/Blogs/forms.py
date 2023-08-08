99427fd0f74fb603004a6a77fc7d50a7e56aad73
uchchhashkhan <uchchhashkhan6@gmail.com>
2023-08-08 11:26:25 +0600

    16th commit

--- aiquest/Blogs/forms.py
+++ aiquest/Blogs/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
 class TeachersRegistration(forms.Form):
-    first_name= forms.CharField()
-    last_name = forms.CharField()
-    email= forms.EmailField()
-    
+    first_name= forms.CharField(label="Enter your first name", label_suffix='')
+    last_name = forms.CharField(initial='Uchchhash' )
+    email= forms.EmailField(initial="uchchhash@gmail.com", disabled='true')
+    password = forms.CharField(widget=forms.PasswordInput)
+    textarea = forms.CharField(widget=forms.Textarea)
+    file = forms.CharField(widget=forms.FileInput)
+    Checkbox = forms.CharField(widget=forms.CheckboxInput)
