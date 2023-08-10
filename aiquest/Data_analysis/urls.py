d6181c955ea5074aac530efa1a2018ee46b47fbb
uchchhashkhan <uchchhashkhan6@gmail.com>
2023-08-11 00:15:00 +0600

    1st commit

--- aiquest/Data_analysis/urls.py
+++ aiquest/Data_analysis/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('d/',views.data_analysis,name='data'),
+    path('class/',views.DataAnalysis.as_view()),
 ]
--- aiquest/Data_analysis/views.py
+++ aiquest/Data_analysis/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def data_analysis(request):
     return render(request,'data_analysis/data_analysis.html')
+
+class DataAnalysis(View):
+     def get(self,request):
+         return render(request,'data_analysis/class.html')
